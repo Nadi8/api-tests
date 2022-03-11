96f7462c2d229157db4e1d05ce69eac6e4f14e3e
Nadi8 <nadia.erdinieva@gmail.com>
2022-03-11 00:22:38 -0500

    update

--- test/posts.js
+++ test/posts.js
@@ -1,47 +1,44 @@
 
 import { expect } from 'chai';
 import supertest from 'supertest';
+
+
+describe('API Tests', function() {
 const request = supertest("https://jsonplaceholder.typicode.com");
 
-describe('Posts', function() {
+   describe('get all users', function() {
+    it('Response status code should be 200', function() {
+        return request
+            .get('/posts').then((res) => {
+                expect(res.statusCode).to.eq(200);         
+        })
+    })
+   
+    it('Users count is 100', function() {
+        return request
+            .get('/posts').then((res) => {
+                expect(res.body.length).to.be.eq(100);                        
+        })
+    })
+   }) 
     
-    it('GET/posts status code should be 200', function() {
-        return request
-            .get('/posts').then((res) => {
-                expect(res.statusCode).to.equal(200);         
-        })
-    })
-
-    it('response body contains list of 2 or more items', function() {
-        return request
-            .get('/posts').then((res) => {
-            expect(res.body.length).to.be.at.least(2);       
-        })
-    })    
-
-    it('id count is 100', function() {
-     return request
-         .get('/posts').then((res) => {
-            expect(res.body.length).to.be.equal(100);                        
-        })
-    })
-
-    it('userId 1 count is 10', function() {
+   describe('userId', function() {
+    it('userId 1 contains 10 ids', function() {
         return request
             .get('/posts?userId=1').then((res) => {
-            expect(res.body.length).to.be.equal(10);                      
+                expect(res.body.length).to.be.eq(10);                      
         })
-    })
-
-    it('Title of id 4', function() {
+    })      
+   }) 
+   
+   describe('id', function() {
+    it('Title of id 4 contains eum et est occaecati', function() {
         return request
             .get('/posts?id=4').then((res) => {
-            expect(res.text).contains('eum et est occaecati');                      
+                expect(res.text).contains('eum et est occaecati');                      
         })
-    })
-
-    
-
+    })    
+   }) 
 })
 
 
